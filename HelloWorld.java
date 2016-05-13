163f19026aaf055dd7fad152ca6fe8ff6c7b5e02
jaortiz9 <ortiz.janine@gmail.com>
2016-05-13 17:03:10 -0600

    Hello World new

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
+class HelloWorld{
 public static void main(String[] args){
 
     if(args.length == 0){
 	System. exit(-3);
     }
 
-    System.out.println("Hello World");
+    System.out.println("Hello Git World");
 }    
